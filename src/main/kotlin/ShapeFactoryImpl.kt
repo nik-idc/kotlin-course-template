212aefbae160a0cbc34dfa3fb710ca0ad051cdad
nik-idc <58989190+nik-idc@users.noreply.github.com>
2021-11-29 18:24:14 +0300

    lab3: Shapes and Shape Factory

--- src/main/kotlin/ShapeFactoryImpl.kt
+++ src/main/kotlin/ShapeFactoryImpl.kt
@@ -4,28 +4,28 @@ class ShapeFactoryImpl : ShapeFactory {
 	private val maxSize = 1000.0
 	
 	override fun createCircle(radius: Double): Circle {
-		if(radius > 0.0)
+		if (radius > 0.0)
 			return Circle(radius)
 		else
 			throw IllegalArgumentException("Cannot create a circle with a less than 0 radius!")
 	}
 	
 	override fun createSquare(side: Double): Square {
-		if(side > 0.0)
+		if (side > 0.0)
 			return Square(side)
 		else
 			throw IllegalArgumentException("Cannot create a square with a less than 0 side!")
 	}
 	
 	override fun createRectangle(width: Double, height: Double): Rectangle {
-		if(width > 0.0 && height > 0.0)
+		if (width > 0.0 && height > 0.0)
 			return Rectangle(width, height)
 		else
 			throw IllegalArgumentException("Cannot create a rectangle with sides less than 0!")
 	}
 	
 	override fun createTriangle(left: Double, right: Double, base: Double): Triangle {
-		if(left > 0.0 && right > 0.0 && base > 0.0)
+		if (left > 0.0 && right > 0.0 && base > 0.0)
 			return Triangle(left, right, base)
 		else
 			throw IllegalArgumentException("Cannot create a triangle with sides less than 0!")
@@ -52,7 +52,7 @@ class ShapeFactoryImpl : ShapeFactory {
 		var left = 0.0
 		var right = 0.0
 		var base = 0.0
-		while(left >= right + base || right >= left + base || base >= right + left) {
+		while (left >= right + base || right >= left + base || base >= right + left) {
 			left = Random.nextDouble(0.0, maxSize)
 			right = Random.nextDouble(0.0, maxSize)
 			base = Random.nextDouble(0.0, maxSize)
