b921d8657f64aa7e29ce5e3738c5fb0ebd2d27ea
nik-idc <58989190+nik-idc@users.noreply.github.com>
2021-11-29 17:45:48 +0300

    lab3: Demo function for lab3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,35 @@
+fun lab3Demo() {
+	val shapeFactory = ShapeFactoryImpl()
+	
+	val shapesList = arrayOf(
+		shapeFactory.createRandomCircle(),
+		shapeFactory.createRandomCircle(),
+		shapeFactory.createRandomSquare(),
+		shapeFactory.createRandomSquare(),
+		shapeFactory.createRandomRectangle(),
+		shapeFactory.createRandomRectangle(),
+		shapeFactory.createRandomTriangle(),
+		shapeFactory.createRandomTriangle(),
+		shapeFactory.createRandomShape(),
+		shapeFactory.createRandomShape(),
+		shapeFactory.createRandomShape(),
+		shapeFactory.createRandomShape(),
+		shapeFactory.createRandomShape(),
+		shapeFactory.createRandomShape(),
+	)
+	
+	var sumPerimeter = 0.0
+	var sumArea = 0.0
+	
+	for(shape: Shape in shapesList) {
+		sumPerimeter += shape.calcPerimeter()
+		sumArea += shape.calcArea()
+	}
+	
+	println("Total perimeter of all shapes in the list is: $sumPerimeter")
+	println("Total area of all shapes in the list is: $sumArea")
+}
+
 fun main() {
-    //do stuff
+	lab3Demo()
 }
