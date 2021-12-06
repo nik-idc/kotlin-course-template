295b8ab1b582bed38e4cb035f8b2a6cf4073fbf6
nik-idc <58989190+nik-idc@users.noreply.github.com>
2021-12-06 21:43:22 +0300

    lab4: Matrix and MutableMatrix classes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -36,6 +36,54 @@ fun lab3Demo() {
 	println("Greatest area is: $greatestArea")
 }
 
+fun lab4Demo() {
+	val mat1 = MutableMatrix(
+		mutableListOf(
+			mutableListOf(2.0, 4.0, -1.0, 2.0),
+			mutableListOf(3.0, 5.0, 2.0, -3.0),
+			mutableListOf(1.0, 0.0, 1.0, -1.0)
+		)
+	)
+	
+	val mat2 = Matrix(
+		mutableListOf(
+			mutableListOf(1.0, 36.0, -1.0, 21.0),
+			mutableListOf(0.0, -25.0, 3.0, 8.0),
+			mutableListOf(12.0, 11.0, 7.0, 5.0)
+		)
+	)
+	
+	val mat3 = Matrix(
+		mutableListOf(
+			mutableListOf(-7.0, 2.0, 14.0),
+			mutableListOf(3.0, 5.0, 12.0),
+			mutableListOf(12.0, 6.0, -9.0),
+			mutableListOf(-5.0, 2.0, -10.0)
+		)
+	)
+	
+	println("First matrix:\n$mat1")
+	println("Second matrix:\n$mat2")
+	println("Sum result:\n" + (mat1 + mat2).toString())
+	println()
+	
+	println("Matrix:\n$mat1")
+	println("Multiply by scalar 5 result:\n" + (mat1 * 5.0).toString())
+	println()
+	
+	println("First matrix:\n$mat1")
+	println("Second matrix:\n$mat3")
+	println("Multiplication result:\n" + (mat1 * mat3).toString())
+	println()
+	
+	println("Matrix:\n$mat1")
+	println("Third element in second row: " + mat1[1, 2])
+	mat1[1,2] = 321.0
+	println("Matrix after change:\n$mat1")
+	println("Third element in second row after change: " + mat1[1, 2])
+	println()
+}
+
 fun main() {
-	lab3Demo()
+	lab4Demo()
 }
