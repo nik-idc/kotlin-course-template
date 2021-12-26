a700440a6f7afdc742ae1744d47b6bbe5a386c37
nik-idc <58989190+nik-idc@users.noreply.github.com>
2021-12-26 13:54:18 +0300

    lab7: Fixed issues

--- src/main/kotlin/lab7/ShapesSerializer.kt
+++ src/main/kotlin/lab7/ShapesSerializer.kt
@@ -1,9 +1,15 @@
+package lab7
+
+import Circle
+import Rectangle
+import Shape
+import Square
+import Triangle
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.modules.*
 import java.io.File
-import java.io.IOException
 
 class ShapesSerializer {
 	private val json = Json {
@@ -19,38 +25,36 @@ class ShapesSerializer {
 	}
 	
 	fun encodeShape(shape: Shape): String = json.encodeToString(shape)
+	fun encodeShapes(shapes: List<Shape>): String = json.encodeToString(shapes)
+	
 	fun decodeShape(shapeStr: String): Shape = json.decodeFromString(shapeStr)
+	fun decodeShapes(shapesStr: String): List<Shape> = json.decodeFromString(shapesStr)
 }
 
 class ShapeFiles {
-	private val serializer = ShapesSerializer()
-	
-	fun encodeShapesToFile(shapes: List<Shape>, path: String) {
-		try {
-			var res = ""
-			for (shape in shapes)
-				res += serializer.encodeShape(shape)
-			File(path).writeText(res)
-		} catch (exc: IOException) {
-			println("Could not write into file: ${exc.message}")
+	private val json = Json {
+		prettyPrint = true
+		serializersModule = SerializersModule {
+			polymorphic(Shape::class) {
+				subclass(Circle::class)
+				subclass(Square::class)
+				subclass(Rectangle::class)
+				subclass(Triangle::class)
+			}
 		}
 	}
 	
+	fun encodeShapesToFile(encodedShapes: String, path: String) {
+		File(path).writeText(encodedShapes)
+	}
+	
+	fun decodeShapeFromFile(path: String): Shape {
+		val text = File(path).readText()
+		return json.decodeFromString(text)
+	}
+	
 	fun decodeShapesFromFile(path: String): List<Shape> {
-		val res = mutableListOf<Shape>()
-		try {
-			var text = File(path).readText()
-			while (text.isNotEmpty()) {
-				// Take substring before '}', while filtering out all the '\r\n' etc.
-				val curObj = text.substringBefore('}').filter { char -> (!" \r\n".contains(char)) } + '}'
-				res.add(serializer.decodeShape(curObj)) // Decode and add
-				text = text.substringAfter('}') // Update text from file
-			}
-		} catch (exc: IllegalArgumentException) {
-			println("It appears a JSON object was in incorrect format: ${exc.message}")
-		} catch (exc: IOException) {
-			println("Could not read from file: ${exc.message}")
-		}
-		return res
+		val text = File(path).readText()
+		return json.decodeFromString(text)
 	}
 }
